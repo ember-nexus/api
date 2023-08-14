c4c0c0e15dc86db15e88f3b4081e57eef6649d7b
Syndesi <soerenklein98@gmail.com>
2023-08-14 15:27:57 +0200

    Rework event system.

---
+++ tests/UnitTests/EventSystem/NormalizedValueToRawValue/Event/NormalizedValueToRawValueEventTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\UnitTests\EventSystem\NormalizedValueToRawValue\Event;
+
+use App\EventSystem\NormalizedValueToRawValue\Event\NormalizedValueToRawValueEvent;
+use PHPUnit\Framework\TestCase;
+
+class NormalizedValueToRawValueEventTest extends TestCase
+{
+    public function testRawValueIsNotInitializedOnCreation(): void
+    {
+        $event = new NormalizedValueToRawValueEvent('normalized value');
+        $this->expectExceptionMessage('Typed property App\EventSystem\NormalizedValueToRawValue\Event\NormalizedValueToRawValueEvent::$rawValue must not be accessed before initialization');
+        $event->getRawValue();
+    }
+
+    public function testNormalizedValueIsCorrectlySet(): void
+    {
+        $event = new NormalizedValueToRawValueEvent('normalized value');
+        $this->assertSame('normalized value', $event->getNormalizedValue());
+    }
+
+    public function testRawValueIsCorrectlySet(): void
+    {
+        $event = new NormalizedValueToRawValueEvent('normalized value');
+        $event->setRawValue('raw value');
+        $this->assertSame('raw value', $event->getRawValue());
+    }
+}
