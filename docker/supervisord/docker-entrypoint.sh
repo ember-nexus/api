c8a4d072cb55f3710e7f1c879a298afc29f3b561
Syndesi <soerenklein98@gmail.com>
2023-08-31 22:11:03 +0200

    Fix dockerfile entrypoint.

--- docker/supervisord/docker-entrypoint.sh
+++ docker/supervisord/docker-entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 
 mkdir -p /var/www/html/var/logs
 touch /var/www/html/var/logs/log.log
-supervisord --nodaemon --configuration /etc/supervisord.conf
+
+if [ -z "$@" ]; then
+  supervisord --nodaemon --configuration /etc/supervisord.conf
+else
+  exec "$@"
+fi
