c0ebdca746cb9378344d545337e333574859fb37
Syndesi <soerenklein98@gmail.com>
2023-09-16 16:29:57 +0200

    Improve documentation.

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
-header('Access-Control-Allow-Methods: GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE');
-header('Allow: GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE');
+header('Access-Control-Allow-Methods: GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE, PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK, UNLOCK');
+header('Allow: GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE, PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK, UNLOCK');
 header('X-Powered-By: Ember-Nexus-API');
 $method = $_SERVER['REQUEST_METHOD'];
 if ('OPTIONS' == $method) {
