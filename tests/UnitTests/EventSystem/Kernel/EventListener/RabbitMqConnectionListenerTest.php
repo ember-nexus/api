267dddbdc6882f4a9cafe3bc485fa9170dfc1cff
Syndesi <soerenklein98@gmail.com>
2023-09-29 22:51:10 +0200

    wip

---
+++ tests/UnitTests/EventSystem/Kernel/EventListener/RabbitMqConnectionListenerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\UnitTests\EventSystem\Kernel\EventListener;
+
+use App\EventSystem\Kernel\EventListener\RabbitMqConnectionListener;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class RabbitMqConnectionListenerTest extends TestCase
+{
+    use ProphecyTrait;
+
+    public function testEvent(): void
+    {
+        $amqpStreamConnection = $this->prophesize(AMQPStreamConnection::class);
+        $amqpStreamConnection->close()->shouldBeCalledOnce()->willReturn();
+        $eventListener = new RabbitMqConnectionListener($amqpStreamConnection->reveal());
+        $eventListener->onKernelTerminate();
+    }
+}
