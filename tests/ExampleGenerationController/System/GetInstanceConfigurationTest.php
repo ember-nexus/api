a3755499a97406c7721637aabbf216ccc97403d7
Syndesi <soerenklein98@gmail.com>
2023-10-08 15:38:07 +0200

    wip

---
+++ tests/ExampleGenerationController/System/GetInstanceConfigurationTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\tests\ExampleGenerationController\System;
+
+use App\Tests\ExampleGenerationController\BaseRequestTestCase;
+
+class GetInstanceConfigurationTest extends BaseRequestTestCase
+{
+    private const PATH_TO_ROOT = __DIR__.'/../../../';
+
+    public function testGetInstanceConfigurationSuccess(): void
+    {
+        $response = $this->runGetRequest('/instance-configuration', null);
+        $documentationHeadersPath = 'docs/api-endpoints/system/get-instance-configuration/200-response-header.txt';
+        $documentationBodyPath = 'docs/api-endpoints/system/get-instance-configuration/200-response-body.json';
+        $this->assertHeadersInDocumentationAreIdenticalToHeadersFromRequest(
+            self::PATH_TO_ROOT,
+            $documentationHeadersPath,
+            $response
+        );
+        $this->assertBodyInDocumentationIsIdenticalToBodyFromRequest(
+            self::PATH_TO_ROOT,
+            $documentationBodyPath,
+            $response,
+            true
+        );
+    }
+}
