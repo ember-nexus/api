eb7b29dfe6cabd32aa38d05c99662db6cbe15376
Syndesi <soerenklein98@gmail.com>
2024-10-27 15:24:00 +0100

    Improve error handling when request contains malformed JSON, closes #343.

---
+++ src/EventSystem/Exception/EventListener/JsonExceptionEventListener.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSystem\Exception\EventListener;
+
+use App\Factory\Exception\Client400BadContentExceptionFactory;
+use Safe\Exceptions\JsonException;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class JsonExceptionEventListener
+{
+    public function __construct(
+        private Client400BadContentExceptionFactory $client400BadContentExceptionFactory
+    ) {
+    }
+
+    /**
+     * @psalm-suppress UndefinedInterfaceMethod
+     */
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $throwable = $event->getThrowable();
+        if (!($throwable instanceof JsonException)) {
+            return;
+        }
+
+        throw $this->client400BadContentExceptionFactory->createFromJsonException($throwable);
+    }
+}
--- tests/UnitTests/Factory/Exception/Client400BadContentExceptionFactoryTest.php
+++ tests/UnitTests/Factory/Exception/Client400BadContentExceptionFactoryTest.php
@@ -8,6 +8,7 @@ use App\Factory\Exception\Client400BadContentExceptionFactory;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
+use Safe\Exceptions\JsonException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class Client400BadContentExceptionFactoryTest extends TestCase
@@ -28,5 +29,24 @@ class Client400BadContentExceptionFactoryTest extends TestCase
         $this->assertSame("Endpoint expects property 'a' to be b, got 'c'.", $exception->getDetail());
         $this->assertSame(null, $exception->getInstance());
         $this->assertSame('', $exception->getMessage());
+    }
+
+    public function testCreateFromJsonException(): void
+    {
+        $urlGenerator = $this->prophesize(UrlGeneratorInterface::class);
+        $urlGenerator->generate(Argument::cetera())->shouldBeCalledOnce()->willReturn('https://mock.dev/123');
+        $factory = new Client400BadContentExceptionFactory($urlGenerator->reveal());
+
+        $jsonException = new JsonException('message');
+
+        $exception = $factory->createFromJsonException($jsonException);
+
+        $this->assertSame(400, $exception->getStatus());
+        $this->assertSame('Bad content', $exception->getTitle());
+        $this->assertSame('https://mock.dev/123', $exception->getType());
+        $this->assertSame('Unable to parse request as JSON. message.', $exception->getDetail());
+        $this->assertSame(null, $exception->getInstance());
+        $this->assertSame('', $exception->getMessage());
+        $this->assertSame($jsonException, $exception->getPrevious());
     }
 }
