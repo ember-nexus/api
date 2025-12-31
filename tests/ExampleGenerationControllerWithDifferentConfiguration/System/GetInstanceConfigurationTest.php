e71df29968be3e9984449deae2c05457aa948eec
Syndesi <soerenklein98@protonmail.com>
2025-12-31 11:14:59 +0100

    split example generation tests with different configuration into separate test step due to issues

--- tests/ExampleGenerationControllerWithDifferentConfiguration/System/GetInstanceConfigurationTest.php
+++ tests/ExampleGenerationControllerWithDifferentConfiguration/System/GetInstanceConfigurationTest.php
@@ -2,36 +2,13 @@
 
 declare(strict_types=1);
 
-namespace App\tests\ExampleGenerationController\System;
+namespace App\tests\ExampleGenerationControllerWithDifferentConfiguration\System;
 
 use App\Tests\ExampleGenerationController\BaseRequestTestCase;
 
-class GetInstanceConfigurationWithDifferentConfigurationTest extends BaseRequestTestCase
+class GetInstanceConfigurationTest extends BaseRequestTestCase
 {
     private const string PATH_TO_ROOT = __DIR__.'/../../../';
-    private const string PATH_TO_CONFIGURATION = __DIR__.'/../../../config/packages/ember_nexus.yaml';
-
-    private string $originalConfiguration = '';
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->originalConfiguration = file_get_contents(self::PATH_TO_CONFIGURATION);
-        file_put_contents(
-            self::PATH_TO_CONFIGURATION,
-            "ember_nexus:\n".
-            "  instanceConfiguration:\n".
-            "    enabled: false\n"
-        );
-        shell_exec('cd ../../.. && php bin/console cache:clear');
-    }
-
-    public function tearDown(): void
-    {
-        file_put_contents(self::PATH_TO_CONFIGURATION, $this->originalConfiguration);
-        shell_exec('cd ../../.. && php bin/console cache:clear');
-        parent::tearDown();
-    }
 
     public function testGetInstanceConfigurationFailure403(): void
     {
