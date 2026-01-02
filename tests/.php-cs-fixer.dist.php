219421f2de22af7cc7f94086bd5f257eaf5e9b52
Syndesi <soerenklein98@protonmail.com>
2026-01-02 16:45:18 +0100

    update php cs fixer config

--- tests/.php-cs-fixer.dist.php
+++ tests/.php-cs-fixer.dist.php
@@ -7,6 +7,7 @@ $finder = (new PhpCsFixer\Finder())
 ;
 
 return (new PhpCsFixer\Config())
+    ->setUnsupportedPhpVersionAllowed(true)
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
