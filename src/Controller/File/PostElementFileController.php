9929eb0de2a2a65bc627dc055a1a2afc1ce03e68
Syndesi <soerenklein98@protonmail.com>
2026-01-03 12:28:51 +0100

    add upload post/put logic

--- src/Controller/File/PostElementFileController.php
+++ src/Controller/File/PostElementFileController.php
@@ -4,19 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller\File;
 
-use App\Factory\Exception\Client404NotFoundExceptionFactory;
-use App\Factory\Exception\Server500LogicExceptionFactory;
 use App\Helper\Regex;
-use App\Response\NoContentResponse;
-use App\Security\AccessChecker;
-use App\Security\AuthProvider;
-use App\Type\AccessType;
+use App\Service\UploadCreationService;
 use Ramsey\Uuid\Rfc4122\UuidV4;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Syndesi\CypherEntityManager\Type\EntityManager as CypherEntityManager;
 
 /**
  * @SuppressWarnings("PHPMD.UnusedFormalParameter")
@@ -24,12 +18,8 @@ use Syndesi\CypherEntityManager\Type\EntityManager as CypherEntityManager;
 class PostElementFileController extends AbstractController
 {
     public function __construct(
-        private AuthProvider $authProvider,
-        private AccessChecker $accessChecker,
-        private CypherEntityManager $cypherEntityManager,
-        private Client404NotFoundExceptionFactory $client404NotFoundExceptionFactory,
-        private Server500LogicExceptionFactory $server500LogicExceptionFactory)
-    {
+        private UploadCreationService $uploadCreationService,
+    ) {
     }
 
     #[Route(
@@ -43,15 +33,7 @@ class PostElementFileController extends AbstractController
     public function postElementFile(string $id, Request $request): Response
     {
         $elementId = UuidV4::fromString($id);
-        $userId = $this->authProvider->getUserId();
 
-        // note: update is used because files are optional parts of already existing elements
-        if (!$this->accessChecker->hasAccessToElement($userId, $elementId, AccessType::UPDATE)) {
-            throw $this->client404NotFoundExceptionFactory->createFromTemplate();
-        }
-
-        $request->getContent(true);
-
-        return new NoContentResponse();
+        return $this->uploadCreationService->handleUploadCreationFromRequest($elementId, $request);
     }
 }
