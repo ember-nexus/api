266588fe81ebb841210cb9820a6dadee8df166b9
Syndesi <soerenklein98@protonmail.com>
2026-02-22 23:25:31 +0100

    fix dockerfile issue, refactor healthcheck command

--- src/Command/HealthcheckCommand.php
+++ src/Command/HealthcheckCommand.php
@@ -47,6 +47,121 @@ class HealthcheckCommand extends Command
         parent::__construct();
     }
 
+    private function getCypherVersion(): string
+    {
+        $cypherInfo = $this->cypherEntityManager->getClient()->runStatement(
+            Statement::create(
+                'CALL dbms.components() YIELD versions, edition UNWIND versions AS version RETURN version, edition;'
+            )
+        );
+        $rawVersion = $cypherInfo->first()->get('version');
+        if (!is_string($rawVersion)) {
+            throw $this->server500LogicExceptionFactory->createFromTemplate(sprintf('Expected cypher response to return property version as string, not %s.', get_debug_type($rawVersion))); // @codeCoverageIgnore
+        }
+        $rawEdition = $cypherInfo->first()->get('edition');
+        if (!is_string($rawEdition)) {
+            throw $this->server500LogicExceptionFactory->createFromTemplate(sprintf('Expected cypher response to return property edition as string, not %s.', get_debug_type($rawEdition))); // @codeCoverageIgnore
+        }
+
+        return sprintf(
+            '%s (%s)',
+            $rawVersion,
+            $rawEdition
+        );
+    }
+
+    private function getMongoVersion(): string
+    {
+        $mongoClient = $this->mongoEntityManager->getClient();
+        $mongoDatabase = $this->mongoEntityManager->getDatabase();
+        if (null === $mongoDatabase) {
+            throw new Exception('Mongo database name can not be null.');
+        }
+        $mongo = $mongoClient->selectDatabase($mongoDatabase)->command(['buildInfo' => 1]);
+
+        return $mongo->toArray()[0]->getArrayCopy()['version'];
+    }
+
+    private function getElasticVersion(): string
+    {
+        $elasticClient = $this->elasticEntityManager->getClient();
+        $elastic = $elasticClient->sendRequest(new Request('GET', '/'));
+
+        /**
+         * @psalm-suppress PossiblyUndefinedMethod
+         *
+         * @phpstan-ignore-next-line
+         */
+        return $elastic->asArray()['version']['number'];
+    }
+
+    private function getRedisVersion(): string
+    {
+        return $this->redisClient->info()['Server']['redis_version'];
+    }
+
+    private function getRabbitMqVersion(): string
+    {
+        return $this->AMQPStreamConnection->getServerProperties()['version'][1];
+    }
+
+    private function getS3Status(): string
+    {
+        $isStorageBucketOnline = $this->s3Client->bucketExists(['Bucket' => $this->emberNexusConfiguration->getFileS3StorageBucket()])->isSuccess();
+        if (!$isStorageBucketOnline) {
+            throw new Exception(sprintf('Unable to connect to bucket %s.', $this->emberNexusConfiguration->getFileS3StorageBucket()));
+        }
+        $isUploadBucketOnline = $this->s3Client->bucketExists(['Bucket' => $this->emberNexusConfiguration->getFileS3UploadBucket()])->isSuccess();
+        if (!$isUploadBucketOnline) {
+            throw new Exception(sprintf('Unable to connect to bucket %s.', $this->emberNexusConfiguration->getFileS3UploadBucket()));
+        }
+
+        return 'online';
+    }
+
+    private function getAlpineVersion(): string
+    {
+        $alpineVersion = \Safe\shell_exec('cat /etc/os-release | grep -i version') ?? '=unknown';
+
+        /**
+         * @psalm-suppress PossiblyUndefinedArrayOffset
+         */
+        return trim(explode('=', $alpineVersion, 2)[1]);
+    }
+
+    private function getCaddyVersion(): string
+    {
+        $caddyVersion = 'unknown';
+        $caddyVersionOutput = explode(' ', \Safe\shell_exec('frankenphp -v 2>&1') ?? '');
+        if (count($caddyVersionOutput) > 1) {
+            $caddyVersion = \Safe\preg_replace('/[^0-9.]/', '', $caddyVersionOutput[0]);
+        }
+
+        /** @var string $caddyVersion */
+        return $caddyVersion;
+    }
+
+    private function getFrankenPhpVersion(): string
+    {
+        $frankenPhpVersion = 'unknown';
+        $frankenPhpVersionOutput = getenv('FRANKENPHP_VERSION');
+        if (is_string($frankenPhpVersionOutput)) {
+            $frankenPhpVersion = \Safe\preg_replace('/[^0-9.]/', '', $frankenPhpVersionOutput);
+        }
+
+        /** @var string $frankenPhpVersion */
+        return $frankenPhpVersion;
+    }
+
+    /**
+     * @psalm-suppress InvalidFalsableReturnType
+     * @psalm-suppress FalsableReturnStatement
+     */
+    private function getPhpVersion(): string
+    {
+        return phpversion();
+    }
+
     /**
      * @psalm-suppress PossiblyUndefinedArrayOffset
      */
@@ -57,124 +172,19 @@ class HealthcheckCommand extends Command
         $this->io->title('Healthcheck');
 
         $this->io->startSection('Check if databases are online');
-
-        $neo4jInfo = $this->cypherEntityManager->getClient()->runStatement(
-            Statement::create(
-                'CALL dbms.components() YIELD versions, edition UNWIND versions AS version RETURN version, edition;'
-            )
-        );
-        $rawVersion = $neo4jInfo->first()->get('version');
-        if (!is_string($rawVersion)) {
-            throw $this->server500LogicExceptionFactory->createFromTemplate(sprintf('Expected cypher response to return property version as string, not %s.', get_debug_type($rawVersion))); // @codeCoverageIgnore
-        }
-        $rawEdition = $neo4jInfo->first()->get('edition');
-        if (!is_string($rawEdition)) {
-            throw $this->server500LogicExceptionFactory->createFromTemplate(sprintf('Expected cypher response to return property edition as string, not %s.', get_debug_type($rawEdition))); // @codeCoverageIgnore
-        }
-        $neo4jVersion = sprintf(
-            '%s (%s)',
-            $rawVersion,
-            $rawEdition
-        );
-
-        $this->io->writeln(sprintf(
-            'Neo4j version:         %s',
-            $neo4jVersion
-        ));
-
-        $mongoClient = $this->mongoEntityManager->getClient();
-        $mongoDatabase = $this->mongoEntityManager->getDatabase();
-        if (null === $mongoDatabase) {
-            throw new Exception('Mongo database name can not be null.');
-        }
-        $mongo = $mongoClient->selectDatabase($mongoDatabase)->command(['buildInfo' => 1]);
-        $mongoVersion = $mongo->toArray()[0]->getArrayCopy()['version'];
-
-        $this->io->writeln(sprintf(
-            'MongoDB version:       %s',
-            $mongoVersion
-        ));
-
-        $elasticClient = $this->elasticEntityManager->getClient();
-        $elastic = $elasticClient->sendRequest(new Request('GET', '/'));
-        /**
-         * @psalm-suppress PossiblyUndefinedMethod
-         *
-         * @phpstan-ignore-next-line
-         */
-        $elasticVersion = $elastic->asArray()['version']['number'];
-
-        $this->io->writeln(sprintf(
-            'Elasticsearch version: %s',
-            $elasticVersion
-        ));
-
-        $redisVersion = $this->redisClient->info()['Server']['redis_version'];
-
-        $this->io->writeln(sprintf(
-            'Redis version:         %s',
-            $redisVersion
-        ));
-
-        $rabbitMqVersion = $this->AMQPStreamConnection->getServerProperties()['version'][1];
-
-        $this->io->writeln(sprintf(
-            'RabbitMQ version:      %s',
-            $rabbitMqVersion
-        ));
-
-        $isStorageBucketOnline = $this->s3Client->bucketExists(['Bucket' => $this->emberNexusConfiguration->getFileS3StorageBucket()])->isSuccess();
-        if (!$isStorageBucketOnline) {
-            throw new Exception(sprintf('Unable to connect to bucket %s.', $this->emberNexusConfiguration->getFileS3StorageBucket()));
-        }
-        $isUploadBucketOnline = $this->s3Client->bucketExists(['Bucket' => $this->emberNexusConfiguration->getFileS3UploadBucket()])->isSuccess();
-        if (!$isUploadBucketOnline) {
-            throw new Exception(sprintf('Unable to connect to bucket %s.', $this->emberNexusConfiguration->getFileS3UploadBucket()));
-        }
-        $this->io->writeln('S3 buckets:            online');
-
+        $this->io->writeln(sprintf('Neo4j version:         %s', $this->getCypherVersion()));
+        $this->io->writeln(sprintf('MongoDB version:       %s', $this->getMongoVersion()));
+        $this->io->writeln(sprintf('Elasticsearch version: %s', $this->getElasticVersion()));
+        $this->io->writeln(sprintf('Redis version:         %s', $this->getRedisVersion()));
+        $this->io->writeln(sprintf('RabbitMQ version:      %s', $this->getRabbitMqVersion()));
+        $this->io->writeln(sprintf('S3 buckets:            %s', $this->getS3Status()));
         $this->io->stopSection('All databases are online.');
 
         $this->io->startSection('Check internal services');
-
-        $alpineVersion = \Safe\shell_exec('cat /etc/os-release | grep -i version') ?? '=unknown';
-        $alpineVersion = trim(explode('=', $alpineVersion, 2)[1]);
-
-        $this->io->writeln(sprintf(
-            'Alpine version:        %s',
-            $alpineVersion
-        ));
-
-        $caddyVersion = 'unknown';
-        $caddyVersionOutput = explode(' ', \Safe\shell_exec('frankenphp -v 2>&1') ?? '');
-        if (count($caddyVersionOutput) > 1) {
-            $caddyVersion = \Safe\preg_replace('/[^0-9.]/', '', $caddyVersionOutput[0]);
-        }
-        /** @var string $caddyVersion */
-        $this->io->writeln(sprintf(
-            'Caddy version:         %s',
-            $caddyVersion
-        ));
-
-        $frankenPhpVersion = 'unknown';
-        $frankenPhpVersionOutput = getenv('FRANKENPHP_VERSION');
-        if (is_string($frankenPhpVersionOutput)) {
-            $frankenPhpVersion = \Safe\preg_replace('/[^0-9.]/', '', $frankenPhpVersionOutput);
-        }
-        /** @var string $frankenPhpVersion */
-        $this->io->writeln(sprintf(
-            'FrankenPHP version:    %s',
-            $frankenPhpVersion
-        ));
-
-        /**
-         * @psalm-suppress PossiblyFalseArgument
-         */
-        $this->io->writeln(sprintf(
-            'PHP version:           %s',
-            phpversion()
-        ));
-
+        $this->io->writeln(sprintf('Alpine version:        %s', $this->getAlpineVersion()));
+        $this->io->writeln(sprintf('Caddy version:         %s', $this->getCaddyVersion()));
+        $this->io->writeln(sprintf('FrankenPHP version:    %s', $this->getFrankenPhpVersion()));
+        $this->io->writeln(sprintf('PHP version:           %s', $this->getPhpVersion()));
         $this->io->stopSection('Internal services are ok.');
 
         $this->io->startSection('Check API');
